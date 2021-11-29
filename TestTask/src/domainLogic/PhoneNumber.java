371a337cfe019fe8d555f11b9e62457eabb5c6e9
Fabian Grobert <fabian.grobert@gmail.com>
2021-11-29 02:39:18 -0300

    Added PhoneNumber Model

---
+++ TestTask/src/domainLogic/PhoneNumber.java
@@ -0,0 +1,69 @@
+package domainLogic;
+
+/**
+ *
+ * @author Fabian
+ */
+public class PhoneNumber {
+
+    private String number;
+    private String countryCode;
+    private String cityCode;
+
+    static final String defaultCityCode = "812";
+    static final String defaultCountryCode = "7";
+
+    public PhoneNumber(String data) {
+        int condition = data.length();
+        switch (condition) {
+            case 11:
+                this.countryCode = data.substring(0, 1);
+                this.cityCode = data.substring(1, 4);
+                this.number = data.substring(4);
+                break;
+            case 10:
+                this.countryCode = defaultCountryCode;
+                this.cityCode = data.substring(0, 3);
+                this.number = data.substring(3);
+                break;
+            case 8:
+                this.countryCode = data.substring(0, 1);
+                this.cityCode = defaultCityCode;
+                this.number = data.substring(1);
+                break;
+            case 7:
+                this.countryCode = defaultCountryCode;
+                this.cityCode = defaultCityCode;
+                this.number = data;
+                break;
+            default:
+                this.countryCode = "";
+                this.cityCode = "";
+                this.number = "";
+                break;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("+%s (%s) %s-%s", countryCode, cityCode, number.substring(0, 3), number.substring(3));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof PhoneNumber)) {
+            return false;
+        }
+
+        PhoneNumber n = (PhoneNumber) o;
+
+        return n.number.equals(this.number) && n.countryCode.equals(this.countryCode) && n.cityCode.equals(this.cityCode);
+    }
+
+}
